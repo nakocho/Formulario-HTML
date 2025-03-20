6b673bca6091ae4e64b7085d697941abac023dc8
Ignacio Hidalgo <ihidalgo2@seidor.es>
2025-03-20 19:19:09 +0100

    Agregar validación de campos requeridos y manejo de clases de error en el formulario

--- funciones.js
+++ funciones.js
@@ -3,13 +3,38 @@
 function validationRowData(row){
     console.log('------- inicio de validación de datos de fila -------');
     console.log(row);
-
     const fieldsRequired = getRequiredFields(row);
-    let a = fieldsRequired.get("dni")
-    console.log("a",a);
-    console.log("ojo",fieldsRequired);
-    console.log('-----------------------------------------------------');
-    
+    if (!fieldsRequired) {
+        console.log('No se encontraron campos requeridos en la fila proporcionada.');
+        return;
+    }
+    const isRequiredComplited =  validationRequiredFields(row, fieldsRequired);
+    return isRequiredComplited;
+}
+
+function validationRequiredFields(row, fieldsRequired){
+    const elementHtml = getElementHtml(row);
+    let requiredComplited = fieldsRequired.every((field) => field.fieldValue);
+    if (requiredComplited) {
+        console.log('Todos los campos requeridos están completos.');
+        $(elementHtml).find('[required]').removeClass("is-invalid");
+        return true;
+    }
+    console.log('Faltan campos requeridos por completar.');
+    $(elementHtml).find('[required]').removeClass("is-invalid");
+
+    fieldsRequired.map((field) => {
+        const { fieldName, fieldValue } = field;
+        if (!fieldValue) {
+            console.log(`El campo "${fieldName}" es requerido.`);
+            $(elementHtml).find(`[name="${fieldName}"]`).addClass("is-invalid");
+            requiredComplited = false;
+        }
+        else{
+            $(elementHtml).find(`[name="${fieldName}"]`).removeClass("is-invalid");
+        }
+    });
+    return requiredComplited;
 }
 
 function getRequiredFields(row) {
@@ -28,7 +53,7 @@ function getRequiredFields(row) {
 function getElementHtml(row){
     const idRow = $(row).attr("id");
     if (!idRow) return;
-    const elementHtml = $(`#${idRow}`).html();
+    const elementHtml = $(`#${idRow}`); // Devuelve el elemento DOM en lugar de su HTML
     return elementHtml;
 
 }
@@ -58,6 +83,7 @@ function setDefaultRowData(row){
 }
 function setDefaultFirstRowData(){
     const firstDate= $(document).find('#idRegistroRow_1');
+    $('.is-invalid').removeClass('is-invalid');
     setDefaultRowData(firstDate);
 } 
 
