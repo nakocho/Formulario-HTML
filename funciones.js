ea71203f787e042399601224d87f1e8b85fb6b1e
Ignacio Hidalgo <ihidalgo2@seidor.es>
2025-03-20 17:39:18 +0100

    Agregar funciones para establecer datos por defecto y enfocar el primer campo en filas de formulario

--- funciones.js
+++ funciones.js
@@ -1,23 +1,11 @@
-function saludosTerrricolas(nombre){
-    if (nombre){
-        console.log('Hola terrícola ' + nombre + '!');
-    }
-    console.log('Hola terrícola!');
-    alert('Hola terrícola!');
-    debugger;
-    const datos = localStorage.getItem('formularioSolicitudes');
-    const filas = JSON.parse(datos);
-    console.log(filas);
-    console.log(datos);
 
-}
 
 function validationRowData(row){
+    console.log('------- inicio de validación de datos de fila -------');
     console.log(row);
     
-
+    console.log('-----------------------------------------------------');
     
-    debugger;
 }
 
 function registroActive(element){
@@ -25,5 +13,45 @@ function registroActive(element){
     if (!idRow) return;
     return idRow;
 }
+
+function defaultRowData(){
+    return {
+        // dni: "22",
+        // nombre: 'J.Philips',
+        // apellidos: 'Fray',
+        // email: 'lover@flower.com',
+        'fecha-inicio': setDateTodayData(),
+        'fecha-final': setDateTodayData(),
+    }
+}
+function setDefaultRowData(row){
+    const defaultData = defaultRowData();
+    Object.keys(defaultData).forEach(key => {
+        row.find('.'+key).val(defaultData[key]);
+    });
+    setFocusFirstField(row);
+}
+function setDefaultFirstRowData(){
+    const firstDate= $(document).find('#idRegistroRow_1');
+    setDefaultRowData(firstDate);
+} 
+
+function setDateTodayData(){
+    const today = new Date();
+    const formattedDate = today.toISOString().slice(0, 16);
+    return formattedDate;
+}
+
+function setFocusFirstField(row){
+    const dniField = row.find('.firstField');
+    if (dniField.length > 0) {
+        setTimeout(() => {
+            dniField.trigger('focus'); // Dar foco al campo .dni
+        }, 0);
+    } else {
+        console.warn('El campo .firstField no se encontró en la fila proporcionada.');
+    }
+}
+
 
 
